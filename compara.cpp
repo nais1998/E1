e3029f84f588b7d0650bc667537b17e1c3f9e00a
nais1998 <anais.napa.ortiz@utelvt.edu.ec>
2022-09-29 17:07:18 -0500

    programa de compara.cpp modificado

---
+++ compara.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+ using namespace std; 
+ int main() 
+ { 
+   float o,r; 
+   cout<<"Ingrese o  : "; cin>>o; 
+   cout<<"Ingrese r  : "; cin>>r; 
+   if(o==r){ 
+      cout<<"Son iguales"<<endl; 
+   }else{ 
+      if(o<r){ 
+        cout<<o<<" es el menor que "<<r<<"\n"; 
+      }else{ 
+        cout<<r<<" es el menor que "<<o<<"\n"; 
+      } 
+   } 
+  
+ return (0);; 
+  
+  
+  
+  
+  
+  
+ } 
+ 
